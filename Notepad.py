8db875ec3d8f9c36b229fce66dc413c5dd956752
Arnav Singhal <singhal.arnav@gmail.com>
2021-12-23 23:42:13 +0530

    Added comments

--- Notepad.py
+++ Notepad.py
@@ -4,12 +4,16 @@ class CreateMenu():
 	def __init__(self,parent):
 		self.menuBar=wx.MenuBar()
 		self.file=wx.Menu()
+
+        # File menu in menu bar
 		self.open=self.file.Append(-1,'Open')
 		self.save=self.file.Append(-1,'Save')
 		self.file.AppendSeparator()
 		self.close=self.file.Append(-1,'Close')
 		self.menuBar.Append(self.file,'File')
 		self.edit=wx.Menu()
+
+        # Edit menu in menu bar
 		self.undo=self.edit.Append(-1,'Undo')
 		self.redo=self.edit.Append(-1,'Redo')
 		self.edit.AppendSeparator()
@@ -19,6 +23,7 @@ class CreateMenu():
 		self.edit.AppendSeparator()
 		self.selectall=self.edit.Append(-1,'Select All')
 		self.menuBar.Append(self.edit,'Edit')
+
 		parent.SetMenuBar(self.menuBar)
 
 
@@ -26,10 +31,14 @@ class MyApp(wx.App):
 
 	def OnInit(self):
 		self.file=''
+
+        # Window sizing and positioning
 		self.frame=wx.Frame(parent=None,title='Pad Master',size=(800,600))
 		self.panel=wx.Panel(self.frame,-1)
 		self.menu=CreateMenu(self.frame)
 		self.text=wx.TextCtrl(self.panel,-1,pos=(2,2),size=(795,580),style=wx.HSCROLL|wx.TE_MULTILINE)
+
+        # Action list
 		self.Bind(wx.EVT_MENU,self.OnOpen,self.menu.open)
 		self.Bind(wx.EVT_MENU,self.OnSave,self.menu.save)
 		self.Bind(wx.EVT_MENU,self.OnClose,self.menu.close)
@@ -39,10 +48,11 @@ class MyApp(wx.App):
 		self.Bind(wx.EVT_MENU,self.OnCopy,self.menu.copy)
 		self.Bind(wx.EVT_MENU,self.OnPaste,self.menu.paste)
 		self.Bind(wx.EVT_MENU,self.OnSelectAll,self.menu.selectall)
+
 		self.frame.Show()
 		return True
 
-
+    # Action implementation
 	def OnOpen(self,event):
 		dialog=wx.FileDialog(None,'Pad Master',style=wx.FD_OPEN)
 		if dialog.ShowModal()==wx.ID_OK:
